6c6b217b1d4c5f7ecd9e7d6acd9734849abefbc5
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-02 00:55:44 +0530

    Program_Pattern_Pyramid

---
+++ General_Java_Coding_Solutions/src/fundamentalsAndPatterns/Program_Pattern_Pyramid.java
@@ -0,0 +1,57 @@
+// Pattern
+/*
+5
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * *
+
+ */
+
+package fundamentalsAndPatterns;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program_Pattern_Pyramid
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		// File IO Start
+		File input = new File("Input.txt");
+		File output = new File("Output.txt");
+		input.createNewFile();
+		output.createNewFile();
+		BufferedReader br = new BufferedReader(new FileReader(input));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(output));
+		// File IO End
+
+		// Actual Program Logic
+		int number = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= number; i++)
+		{
+
+			for (int j = 1; j <= number - i; j++)
+			{
+				bw.write("  ");
+			}
+			for (int j = 1; j <= (2 * i) - 1; j++)
+			{
+				bw.write("* ");
+			}
+
+			bw.write("\n");
+
+		}
+		// logic end here
+		br.close();
+		bw.close();
+	}
+
+}
