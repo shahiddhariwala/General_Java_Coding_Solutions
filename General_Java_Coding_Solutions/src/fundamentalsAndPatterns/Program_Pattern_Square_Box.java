7a8d58302d6f3254aa27b06b856d2c35c88b979b
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-01 22:32:30 +0530

    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * patern

---
+++ General_Java_Coding_Solutions/src/fundamentalsAndPatterns/Program_Pattern_Square_Box.java
@@ -0,0 +1,47 @@
+package fundamentalsAndPatterns;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program_Pattern_Square_Box
+{
+// Pattern
+//	* * * * * * 
+//	* * * * * * 
+//	* * * * * * 
+//	* * * * * * 
+//	* * * * * * 
+//	* * * * * * 
+
+	public static void main(String[] args) throws IOException
+	{
+		// File IO Start
+		File input = new File("Input.txt");
+		File output = new File("Output.txt");
+		input.createNewFile();
+		output.createNewFile();
+		BufferedReader br = new BufferedReader(new FileReader(input));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(output));
+		// File IO End
+
+		// Actual Program Logic
+		int number = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= number; i++)
+		{
+			for (int j = 1; j <= number; j++)
+			{
+				bw.write("* ");
+			}
+			bw.write("\n");
+
+		}
+		// logic end here
+		br.close();
+		bw.close();
+	}
+
+}
