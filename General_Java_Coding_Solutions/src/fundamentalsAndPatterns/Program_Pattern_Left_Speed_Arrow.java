b68fd6189b9bf70f1eee8afe9ec5c65e358153bf
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-02 19:58:27 +0530

    Program_Pattern_Left_Speed_Arrow

---
+++ General_Java_Coding_Solutions/src/fundamentalsAndPatterns/Program_Pattern_Left_Speed_Arrow.java
@@ -0,0 +1,63 @@
+// Pattern
+/*
+===========================================================================
+5
+        * * * * * 
+      * * * * 
+    * * * 
+  * * 
+* 
+  * * 
+    * * * 
+      * * * * 
+        * * * * * 
+===========================================================================
+
+
+ */
+
+package fundamentalsAndPatterns;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Program_Pattern_Left_Speed_Arrow
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("===========================================================================");
+		// Actual Program Logic
+		int n = Integer.parseInt(br.readLine());
+
+		for (int row = 1, nst = n, nsp = n - 1; row <= (2 * n) - 1; row++)
+		{
+			for (int csp = 1; csp <= nsp; csp++)
+			{
+				System.out.print("  ");
+			}
+			for (int cst = 1; cst <= nst; cst++)
+			{
+				System.out.print("* ");
+			}
+			if (row < n)
+			{
+				nst -= 1;
+				nsp -= 1;
+			} else
+			{
+				nst += 1;
+				nsp += 1;
+			}
+
+			System.out.println();
+		}
+		System.out.println("===========================================================================");
+		br.close();
+		// logic end here
+
+	}
+
+}
