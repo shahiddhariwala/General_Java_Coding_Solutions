504a0dd4bebe4e1f09501c5facd06aeb4751c04e
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-05 20:53:23 +0530

    Program_To_Calculate_Quadratic_Equation

---
+++ General_Java_Coding_Solutions/src/challenging_Programs_Fundamentals_and_Patterns/Program_To_Calculate_Quadratic_Equation.java
@@ -0,0 +1,90 @@
+/*
+Coded by : Shahid Dhariwala
+LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
+Twitter  : https://twitter.com/shahiddhariwala
+*/
+
+/*
+Given coefficients of a quadratic equation , you need to print the nature of the roots 
+(Real and Distinct , Real and Equal or Imaginary) and the roots.
+If Real and Distinct , print the roots in increasing order.
+If Real and Equal , print the same repeating root twice
+If Imaginary , no need to print the roots.
+
+Note : Print only the integer part of the roots.
+
+Input Format
+First line contains three integer coefficients a,b,c for the equation ax^2 + bx + c = 0.
+
+Constraints
+-100 <= a, b, c <= 100
+
+Output Format
+Output contains one/two lines. First line contains nature of the roots .
+The next line contains roots(in non-decreasing order) separated by a space if they exist. 
+If roots are imaginary do not print the roots. Output the integer values for the roots.
+
+Sample Input
+1 -11 28
+Sample Output
+Real and Distinct
+4 7
+Explanation
+The input corresponds to equation ax^2 + bx + c = 0. Roots = (-b + sqrt(b^2 - 4ac))/2a , 
+(-b - sqrt(b^2 - 4ac))/2a
+ */
+package challenging_Programs_Fundamentals_and_Patterns;
+
+import java.util.Scanner;
+
+public class Program_To_Calculate_Quadratic_Equation {
+/*
+ * Quick Info
+ * The discriminant, b2 - 4ac, offers valuable information about the
+ *  "nature" of the roots of a quadratic equation where a, b and c are rational values. 
+ *  It quickly tells you if the equation has two real roots (b2 - 4ac > 0), 
+ *  one real repeated root (b2 - 4ac = 0) or two complex conjugate roots (b2 - 4ac < 0). 
+ *  If you are trying to determine the "type" of roots of a quadratic equation (not the actual roots themselves),
+ *  you need not complete the entire quadratic formula. Simply look at the discriminant.
+ */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		//1 -11 28
+		System.out.printf("Eqaution is : %dx^2 + (%d)x + %d = 0 \n",a,b,c);
+		int bb = (int)Math.pow(b, 2);
+		int _2a = 2*a;
+		int _4ac = 4*a*c;
+		
+		int discriminant = bb-_4ac;
+		//check discriminat to determine the nature of roots
+		if(discriminant<0)
+		{
+			System.out.println("Imaginary");
+		}
+		else
+		{
+			int rootPos=(int) ((-b+Math.sqrt(bb-_4ac))/_2a);
+			int rootNeg=(int) ((-b-Math.sqrt(bb-_4ac))/_2a);
+			
+			if(rootPos==rootNeg)
+			{
+				System.out.println("Real and Equal");
+				System.out.println(rootPos+ " "+rootNeg);
+			}
+			else
+			{
+				System.out.println("Real and Distinct");
+				int max = Math.max(rootNeg, rootPos);
+				int min = Math.min(rootNeg,rootPos);
+				System.out.println(min+ " "+max);
+			}
+		}
+		
+		sc.close();
+	}
+}
+
+/*https://github.com/shahiddhariwala*/
--- General_Java_Coding_Solutions/src/numberSystemAndDataType/Program_To_Convert_Fahrenheit_To_Celcius.java
+++ General_Java_Coding_Solutions/src/numberSystemAndDataType/Program_To_Convert_Fahrenheit_To_Celcius.java
@@ -1,5 +1,22 @@
 package numberSystemAndDataType;
 
+/*F = 0	C = -17
+F = 20	C = -6
+F = 40	C = 4
+F = 60	C = 15
+F = 80	C = 26
+F = 100	C = 37
+F = 120	C = 48
+F = 140	C = 60
+F = 160	C = 71
+F = 180	C = 82
+F = 200	C = 93
+F = 220	C = 104
+F = 240	C = 115
+F = 260	C = 126
+F = 280	C = 137
+F = 300	C = 148
+*/
 public class Program_To_Convert_Fahrenheit_To_Celcius {
 
 	public static void main(String[] args) {
