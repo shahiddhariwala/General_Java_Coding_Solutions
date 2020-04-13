ef4e2b863b66f935573f1940acfa1dc1d9b61996
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-13 17:44:22 +0530

    Factorial using Recursion

---
+++ General_Java_Coding_Solutions/src/recursionAndBacktracking/Program_To_Print_Factorial_Using_Recursion.java
@@ -0,0 +1,36 @@
+/*
+Code by  : Shahid Dhariwala
+LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
+Twitter  : https://twitter.com/shahiddhariwala
+*/
+
+
+package recursionAndBacktracking;
+
+public class Program_To_Print_Factorial_Using_Recursion
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(getFactorial(5));
+
+	}
+
+	private static int getFactorial(int n)
+	{
+		if(n==0 || n==1)
+		{
+			//base case
+			return 1;
+		}
+		else
+		{
+			return n*getFactorial(n-1);
+		}
+	}
+
+}
+
+
+
+/*https://github.com/shahiddhariwala*/
