a904c73c97c290832833dedb77972ef90248fefa
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-13 17:54:29 +0530

    Calculate_Power_Using_Recursion

---
+++ General_Java_Coding_Solutions/src/recursionAndBacktracking/Program_To_Calculate_Power_Using_Recursion.java
@@ -0,0 +1,31 @@
+/*
+Code by  : Shahid Dhariwala
+LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
+Twitter  : https://twitter.com/shahiddhariwala
+*/
+
+
+package recursionAndBacktracking;
+
+public class Program_To_Calculate_Power_Using_Recursion
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(power(2,4));
+	}
+	private static int power(int x, int n)
+	{
+		if(n==0)
+		{
+			//handles base case x^0 = 1
+			return 1;
+		}
+		return x*(power(x,n-1));
+	}
+
+}
+
+
+
+/*https://github.com/shahiddhariwala*/
