2027495e50afe53866ef267fe82e107f4cbd32f5
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-13 22:31:15 +0530

    Printing all subsequences combination of String

---
+++ General_Java_Coding_Solutions/src/recursionAndBacktracking/Program_To_Get_Subsequences_ArrayList_Using_Recursion.java
@@ -0,0 +1,51 @@
+/*
+Code by  : Shahid Dhariwala
+LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
+Twitter  : https://twitter.com/shahiddhariwala
+*/
+
+
+package recursionAndBacktracking;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Program_To_Get_Subsequences_ArrayList_Using_Recursion
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String str = "abc";
+		ArrayList<String> al = getSubsequences(str);
+		System.out.println(al);
+		sc.close();
+		
+
+	}
+
+	private static ArrayList<String> getSubsequences(String str)
+	{
+		ArrayList<String> al;
+		if(str.equals(""))
+		{
+			al = new  ArrayList<String>();
+			al.add("");
+			return al;
+		}
+		char ch = str.charAt(0);
+		al = getSubsequences(str.substring(1));
+		ArrayList<String> myResult = new ArrayList<String>();
+		for (String s : al)
+		{
+			myResult.add(s);
+			myResult.add(ch+s);
+		}
+		return myResult;
+	}
+
+}
+
+
+
+/*https://github.com/shahiddhariwala*/
