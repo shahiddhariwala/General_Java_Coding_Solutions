1fe278e7351fba35be9ccc395c57fb8a18d8bc6a
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-15 18:44:57 +0530

    Quick SOrt ALgorithm

--- General_Java_Coding_Solutions/src/recursionAndBacktracking/Program_To_Sort_Array_using_Quick_Sort.java
+++ General_Java_Coding_Solutions/src/recursionAndBacktracking/Program_To_Sort_Array_using_Quick_Sort.java
@@ -17,33 +17,33 @@ public class Program_To_Sort_Array_using_Quick_Sort
 		{ 9, 8, 7, 6, 25, 20, 5, 4, 3, 10, 15, 2, 1 };
 		doQuickSort(arr, 0, arr.length - 1);
 		System.out.println(Arrays.toString(arr));
-		//Quick SOrt is inplace algorithm as it does not require extra space
-		//[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25]
+		// Quick SOrt is inplace algorithm as it does not require extra space
+		// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25]
 	}
 
 	private static void doQuickSort(int[] arr, int start, int end)
 	{
-		if(start<=end)
+		if (start <= end)
 		{
-			//1. Partitioning
-			int pivotIndex = doPartition(arr,start,end);
-			//pivotIndex is our item which has all elements at it left smaller 
-			//and all thge element to right of it is greater than it
-			
-			//2. Work on Smaller Problem
-			doQuickSort(arr,start,pivotIndex-1);
-			doQuickSort(arr,pivotIndex+1,end);
+			// 1. Partitioning
+			int pivotIndex = doPartition(arr, start, end);
+			// pivotIndex is our item which has all elements at it left smaller
+			// and all thge element to right of it is greater than it
+
+			// 2. Work on Smaller Problem
+			doQuickSort(arr, start, pivotIndex - 1);
+			doQuickSort(arr, pivotIndex + 1, end);
 		}
-				
+
 	}
 
 	private static int doPartition(int[] arr, int start, int end)
 	{
-		int pi=end;
-		int i=start-1;
+		int pi = end;
+		int i = start - 1;
 		for (int j = start; j <= end; j++)
 		{
-			if(arr[j]<=arr[pi])
+			if (arr[j] <= arr[pi])
 			{
 				i++;
 				int temp = arr[j];
@@ -51,7 +51,7 @@ public class Program_To_Sort_Array_using_Quick_Sort
 				arr[i] = temp;
 			}
 		}
-		
+
 		return i;
 	}
 
