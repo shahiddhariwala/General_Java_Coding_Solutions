16676c3eb1ceb9038df4102307683bfedcd0c8f2
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-21 13:45:54 +0530

    Reverse Stack without Extra Stack

--- General_Java_Coding_Solutions/src/challenging_Programs_On_Stack_And_Queue/Program_to_Reverse_Stack_Using_Recursion_And_Without_Extra_Stack.java
+++ General_Java_Coding_Solutions/src/challenging_Programs_On_Stack_And_Queue/Program_to_Reverse_Stack_Using_Recursion_And_Without_Extra_Stack.java
@@ -3,6 +3,32 @@ Code by  : Shahid Dhariwala
 LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
 Twitter  : https://twitter.com/shahiddhariwala
 */
+
+/*Reverse a Stack using Recursion. Do not use any extra stack.
+
+Input Format
+First line contains an integer N (size of the stack).
+Next N lines follow each containing an integer to be stored in the stack where the last integer is at the top of the stack.
+
+Constraints
+1 <= N <= 10^4
+
+Output Format
+Output the values of the reversed stack with each value in one line each.
+
+Sample Input
+3
+3
+2
+1
+Sample Output
+3
+2
+1
+Explanation
+Original Stack = [ 3 , 2 , 1 ] <-TOP
+Reverse Stack = [ 1 , 2 , 3 ] <-TOP
+*/
 package challenging_Programs_On_Stack_And_Queue;
 
 import java.util.Scanner;
@@ -107,23 +133,36 @@ public class Program_to_Reverse_Stack_Using_Recursion_And_Without_Extra_Stack
 		{
 			s1.push(sc.nextInt());
 		}
-		s1.display();
-		reverseStack(s1, 0, 0);
+		//s1.display();
+		reverseStack(s1);
 		s1.display();
 		sc.close();
 
 	}
 
-	private static int reverseStack(StackA s1, int index, int item) throws Exception
+	private static void reverseStack(StackA s1) throws Exception
 	{
 		if (s1.isEmpty())
 		{
-			return item;
+			return;
 		}
-		int itemo = s1.pop();
-		int rr=reverseStack(s1, index+1, itemo);
-		s1.push(rr);
-		return itemo;
+		int item = s1.pop();
+		reverseStack(s1);
+		insertAtBottom(s1, item);
+
+	}
+
+	private static void insertAtBottom(StackA s1, int item) throws Exception
+	{
+		if (s1.isEmpty())
+		{
+			s1.push(item);
+			return;
+		}
+
+		int ito = s1.pop();
+		insertAtBottom(s1, item);
+		s1.push(ito);
 
 	}
 
