a77d927b3e10c7db2ea52421bf69254aca874b2c
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-24 23:24:16 +0530

    Perfect LinkedList

--- General_Java_Coding_Solutions/src/linkedList/LinkedList.java
+++ General_Java_Coding_Solutions/src/linkedList/LinkedList.java
@@ -24,6 +24,7 @@ public class LinkedList
 		}
 	}
 
+	// O(n)
 	public void display()
 	{
 		Node temp = this.head;
@@ -37,6 +38,7 @@ public class LinkedList
 
 	}
 
+	// O(1)
 	public void addLast(int val)
 	{
 		Node newNode = new Node(val);
@@ -54,6 +56,7 @@ public class LinkedList
 
 	}
 
+	// O(1)
 	public void addFirst(int val)
 	{
 		Node newNode = new Node(val);
@@ -71,11 +74,13 @@ public class LinkedList
 
 	}
 
+	// O(1)
 	public int size()
 	{
 		return this.size;
 	}
 
+	// O(1)
 	public int getFirst() throws Exception
 	{
 		if (size == 0)
@@ -85,6 +90,7 @@ public class LinkedList
 		return this.head.data;
 	}
 
+	// O(1)
 	public int getLast() throws Exception
 	{
 		if (size == 0)
@@ -94,6 +100,7 @@ public class LinkedList
 		return this.tail.data;
 	}
 
+	// O(n)
 	public int getAt(int i) throws Exception
 	{
 		if (size == 0)
@@ -113,6 +120,7 @@ public class LinkedList
 		return temp.data;
 	}
 
+	// O(n)
 	private Node getNodeAt(int index) throws Exception
 	{
 		if (size == 0)
@@ -129,11 +137,182 @@ public class LinkedList
 		}
 		return temp;
 	}
-	
-	public void addAt(int index)
+
+	// O(n)
+	public void addAt(int index, int data) throws Exception
 	{
-		
+
+		if (index > size || index < 0)
+		{
+			throw new Exception(index + " Index Out of Bound Exception  " + this.size);
+		}
+
+		// Lets put our Node
+		if (index == 0)
+		{
+			this.addFirst(data);
+		} else if (index == this.size)
+		{
+			this.addLast(data);
+		} else
+		{
+			// Create New Node
+			Node newNode = new Node(data);
+			// Gets Nodes before Index and Node on that index
+			Node prevNode = this.getNodeAt(index - 1);
+			Node indexNode = prevNode.next;
+
+			// Insert Node between them
+			prevNode.next = newNode;
+			newNode.next = indexNode;
+			this.size++;
+		}
+
 	}
+
+	// O(1)
+	public int removeFirst() throws Exception
+	{
+		if (this.size == 0)
+		{
+			throw new Exception("LinkedList is Empty");
+		}
+		Node temp = head;
+		if (this.size == 1)
+		{
+			this.head = null;
+			this.tail = null;
+		} else
+		{
+			this.head = temp.next;
+		}
+		this.size--;
+		return temp.data;
+	}
+
+	// O(n)
+	public int removeLast() throws Exception
+	{
+		if (this.size == 0)
+		{
+			throw new Exception("LinkedList is Empty");
+		}
+		Node temp = tail;
+		if (this.size == 1)
+		{
+			this.head = null;
+			this.tail = null;
+		} else
+		{
+			this.tail = this.getNodeAt(this.size - 2);
+			this.tail.next = null;
+		}
+		this.size--;
+		return temp.data;
+	}
+
+	// O(n)
+	public int removeAt(int index) throws Exception
+	{
+		if (index > size || index < 0)
+		{
+			throw new Exception(index + " Index Out of Bound Exception  " + this.size);
+		}
+
+		if (index == 0)
+		{
+			return this.removeFirst();
+		} else if (index == this.size - 1)
+		{
+			return this.removeLast();
+		} else
+		{
+			Node prevIndexNode = this.getNodeAt(index - 1);
+			Node currentNode = prevIndexNode.next;
+			prevIndexNode.next = currentNode.next;
+			this.size--;
+			return currentNode.data;
+		}
+	}
+
+	public void reverseData() throws Exception
+	{
+		if (this.size == 0)
+		{
+			throw new Exception("LinkedList is Empty");
+		}
+
+		if (this.size == 1)
+		{
+			return;
+		} else
+		{
+			int left = 0;
+			int right = this.size - 1;
+			while (left < right)
+			{
+				Node leftNode = getNodeAt(left);
+				Node rightNode = getNodeAt(right);
+
+				int temp = leftNode.data;
+				leftNode.data = rightNode.data;
+				rightNode.data = temp;
+				left++;
+				right--;
+			}
+		}
+	}
+
+	public void reversePointer() throws Exception
+	{
+		if (this.size == 0)
+		{
+			throw new Exception("LinkedList is Empty");
+		}
+
+		if (this.size == 1)
+		{
+			return;
+		} else
+		{
+			Node prev = this.head;
+			Node curr = prev.next;
+
+			while (curr != null)
+			{
+				Node ahead = curr.next;
+				curr.next = prev;
+				prev = curr;
+				curr = ahead;
+			}
+
+			// Swap head and tails
+			Node temp = this.head;
+			this.head = this.tail;
+			this.tail = temp;
+			tail.next = null;
+		}
+	}
+
+	public int getMid() throws Exception
+	{
+		if (this.size == 0)
+		{
+			throw new Exception("LinkedList is Empty");
+		}
+
+		Node slow = this.head;
+		Node fast=this.head;
+		
+		
+		while(fast.next != null && fast.next.next != null )
+		{
+			slow=slow.next;
+			fast=fast.next.next;
+		}
+		return slow.data;
+	}
+
 }
 
 /* https://github.com/shahiddhariwala */
--- General_Java_Coding_Solutions/src/linkedList/LinkedList_Client.java
+++ General_Java_Coding_Solutions/src/linkedList/LinkedList_Client.java
@@ -16,19 +16,144 @@ public class LinkedList_Client
 		list.addLast(20);
 		list.addLast(30);
 		list.display();
-		System.out.println("\tSize : "+list.size());
-		System.out.println("\tFirst Item : "+list.getFirst());
-		System.out.println("\tLast Item : "+list.getLast());
-		
+		System.out.println("\tSize : " + list.size());
+		System.out.println("\tFirst Item : " + list.getFirst());
+		System.out.println("\tLast Item : " + list.getLast());
+		/*
+		 * 10 20 30
+		 * Size : 3
+		 * First Item : 10
+		 * Last Item : 30
+		 */
 		list.addFirst(85);
 		list.addFirst(133);
 		list.addLast(101);
 		list.display();
-		System.out.println("\tSize : "+list.size());
-		System.out.println("\tFirst Item : "+list.getFirst());
-		System.out.println("\tLast Item : "+list.getLast());
-		System.out.println("\tItem at 2nd Position is "+list.getAt(2));
-		
+		System.out.println("\tSize : " + list.size());
+		System.out.println("\tFirst Item : " + list.getFirst());
+		System.out.println("\tLast Item : " + list.getLast());
+		System.out.println("\tItem at 2nd Position is " + list.getAt(2));
+		/*
+		 * 133 85 10 20 30 101
+		 * Size : 6
+		 * First Item : 133
+		 * Last Item : 101
+		 * Item at 2nd Position is 10
+		 */
+		list.addAt(2, 1020);
+		list.addAt(0, 1);
+		list.addAt(list.size(), Integer.MAX_VALUE);
+		list.display();
+		System.out.println("\tSize : " + list.size());
+		list.display();
+		System.out.println("\tRemoved " + list.removeFirst());
+		System.out.println("\tRemoved " + list.removeFirst());
+		System.out.println("\tSize : " + list.size());
+		/*
+		 * 1 133 85 1020 10 20 30 101 2147483647
+		 * Size : 9
+		 * 1 133 85 1020 10 20 30 101 2147483647
+		 * Removed 1
+		 * Removed 133
+		 * Size : 7
+		 */
+		list.display();
+		System.out.println("\tRemoved " + list.removeLast());
+		System.out.println("\tRemoved " + list.removeLast());
+		System.out.println("\tRemoved " + list.removeLast());
+		System.out.println("\tRemoved " + list.removeLast());
+		System.out.println("\tRemoved " + list.removeLast());
+		System.out.println("\tRemoved " + list.removeLast());
+		System.out.println("\tRemoved " + list.removeLast());
+		System.out.println("\tSize : " + list.size());
+		list.display();
+		/*
+		 * 85 1020 10 20 30 101 2147483647
+		 * Removed 2147483647
+		 * Removed 101
+		 * Removed 30
+		 * Removed 20
+		 * Removed 10
+		 * Removed 1020
+		 * Removed 85
+		 * Size : 0
+		 * 
+		 */
+		list.addLast(10);
+		list.addLast(20);
+		list.addLast(40);
+		list.addAt(2, 30);
+		list.addLast(50);
+		list.addLast(70);
+		list.addLast(60);
+		list.display();
+		System.out.println("\tSize : " + list.size());
+		System.out.println("\tRemoved " + list.removeAt(5));
+		System.out.println("\tRemoved " + list.removeAt(0));
+		System.out.println("\tRemoved " + list.removeAt(0));
+		System.out.println("\tRemoved " + list.removeAt(list.size() - 1));
+		System.out.println("\tRemoved " + list.removeAt(list.size() - 1));
+		list.display();
+		System.out.println("\tSize : " + list.size());
+		System.out.println("\tRemoved " + list.removeAt(0));
+		System.out.println("\tRemoved " + list.removeAt(list.size() - 1));
+		/*
+		 * 
+		 * 10 20 30 40 50 70 60
+		 * Size : 7
+		 * Removed 70
+		 * Removed 10
+		 * Removed 20
+		 * Removed 60
+		 * Removed 50
+		 * 30 40
+		 * Size : 2
+		 * Removed 30
+		 * Removed 40
+		 */
+		for (int i = 1; i <= 10; i++)
+		{
+			list.addLast(i * 10);
+		}
+		list.display();
+		System.out.println("\tSize : " + list.size());
+		System.out.println("\tFirst Item : " + list.getFirst());
+		System.out.println("\tLast Item : " + list.getLast());
+		list.reverseData();
+		list.display();
+		System.out.println("\tSize : " + list.size());
+		System.out.println("\tFirst Item : " + list.getFirst());
+		System.out.println("\tLast Item : " + list.getLast());
+		list.reversePointer();
+		list.display();
+		System.out.println("\tSize : " + list.size());
+		System.out.println("\tFirst Item : " + list.getFirst());
+		System.out.println("\tLast Item : " + list.getLast());
+		System.out.println("\tMiddle Item : " + list.getMid());
+		System.out.println("\tRemoved " + list.removeLast());
+		list.display();
+		System.out.println("\tSize : " + list.size());
+		System.out.println("\tMiddle Item : " + list.getMid());
+		/*
+		 * 10 20 30 40 50 60 70 80 90 100
+		 * Size : 10
+		 * First Item : 10
+		 * Last Item : 100
+		 * 100 90 80 70 60 50 40 30 20 10
+		 * Size : 10
+		 * First Item : 100
+		 * Last Item : 10
+		 * 10 20 30 40 50 60 70 80 90 100
+		 * Size : 10
+		 * First Item : 10
+		 * Last Item : 100
+		 * Middle Item : 50
+		 * Removed 100
+		 * 10 20 30 40 50 60 70 80 90
+		 * Size : 9
+		 * Middle Item : 50
+		 */
+
 	}
 
 }
