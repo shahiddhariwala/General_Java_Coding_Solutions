5b8efd1a876adf0098a224a917f4ba30721c3a82
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-25 00:45:17 +0530

    Incomplete Merge Sorted LinkedList

---
+++ General_Java_Coding_Solutions/src/linkedList/challenging_Program_On_LinkedList/Program_To_Merge_Sorted_LinkedList.java
@@ -0,0 +1,429 @@
+/*
+Code by  : Shahid Dhariwala
+LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
+Twitter  : https://twitter.com/shahiddhariwala
+*/
+/*
+Given 2 sorted linked lists , merge the two given sorted linked list and print the final Linked List.
+
+Input Format
+First Line contains T the number of test cases.
+For each test case :
+Line 1 : N1 the size of list 1
+Line 2 : N1 elements for list 1
+Line 3 : N2 the size of list 2
+Line 4 : N2 elements for list 2
+
+Constraints
+1 <= T <= 1000
+0<= N1, N2 <= 10^6
+-10^7 <= Ai <= 10^7
+
+Output Format
+For each testcase , print the node data of merged linked list.
+
+Sample Input
+1
+4
+1 3 5 7
+3
+2 4 6
+Sample Output
+1 2 3 4 5 6 7 
+Explanation
+The two lists after merging give the sorted output as [1,2,3,4,5,6,7] .
+*/
+package linkedList.challenging_Program_On_LinkedList;
+
+import java.util.Scanner;
+
+public class Program_To_Merge_Sorted_LinkedList
+{
+	static class LinkedList
+	{
+		private Node head;
+		private Node tail;
+		private int size;
+
+		private class Node
+		{
+			int data;
+			Node next;
+
+			public Node(int data)
+			{
+				this.data = data;
+				this.next = null;
+			}
+		}
+
+		// O(n)
+		public void display()
+		{
+			Node temp = this.head;
+
+			while (temp != null)
+			{
+				System.out.print(temp.data + " ");
+				temp = temp.next;
+			}
+			System.out.println();
+
+		}
+
+		// O(1)
+		public void addLast(int val)
+		{
+			Node newNode = new Node(val);
+
+			if (this.size == 0)
+			{
+				this.head = newNode;
+				this.tail = newNode;
+				this.size += 1;
+				return;
+			}
+			this.size += 1;
+			this.tail.next = newNode;
+			this.tail = newNode;
+
+		}
+
+		// O(1)
+		public void addFirst(int val)
+		{
+			Node newNode = new Node(val);
+
+			if (this.size == 0)
+			{
+				this.head = newNode;
+				this.tail = newNode;
+				this.size += 1;
+				return;
+			}
+			this.size += 1;
+			newNode.next = head;
+			this.head = newNode;
+
+		}
+
+		// O(1)
+		public int size()
+		{
+			return this.size;
+		}
+
+		// O(1)
+		public int getFirst() throws Exception
+		{
+			if (size == 0)
+			{
+				throw new Exception("LinkedList is Empty");
+			}
+			return this.head.data;
+		}
+
+		// O(1)
+		public int getLast() throws Exception
+		{
+			if (size == 0)
+			{
+				throw new Exception("LinkedList is Empty");
+			}
+			return this.tail.data;
+		}
+
+		// O(n)
+		public int getAt(int i) throws Exception
+		{
+			if (size == 0)
+			{
+				throw new Exception("LinkedList is Empty");
+			} else if (i > size - 1 || i < 0)
+			{
+				throw new Exception(i + " Index Out of Bound Exception  " + this.size);
+			}
+			Node temp = head;
+			int count = 0;
+			while (count != i)
+			{
+				temp = temp.next;
+				count++;
+			}
+			return temp.data;
+		}
+
+		// O(n)
+		private Node getNodeAt(int index) throws Exception
+		{
+			if (size == 0)
+			{
+				throw new Exception("LinkedList is Empty");
+			} else if (index > size - 1 || index < 0)
+			{
+				throw new Exception(index + " Index Out of Bound Exception  " + this.size);
+			}
+			Node temp = head;
+			for (int i = 0; i < index; i++)
+			{
+				temp = temp.next;
+			}
+			return temp;
+		}
+
+		// O(n)
+		public void addAt(int index, int data) throws Exception
+		{
+
+			if (index > size || index < 0)
+			{
+				throw new Exception(index + " Index Out of Bound Exception  " + this.size);
+			}
+
+			// Lets put our Node
+			if (index == 0)
+			{
+				this.addFirst(data);
+			} else if (index == this.size)
+			{
+				this.addLast(data);
+			} else
+			{
+				// Create New Node
+				Node newNode = new Node(data);
+				// Gets Nodes before Index and Node on that index
+				Node prevNode = this.getNodeAt(index - 1);
+				Node indexNode = prevNode.next;
+
+				// Insert Node between them
+				prevNode.next = newNode;
+				newNode.next = indexNode;
+				this.size++;
+			}
+
+		}
+
+		// O(1)
+		public int removeFirst() throws Exception
+		{
+			if (this.size == 0)
+			{
+				throw new Exception("LinkedList is Empty");
+			}
+			Node temp = head;
+			if (this.size == 1)
+			{
+				this.head = null;
+				this.tail = null;
+			} else
+			{
+				this.head = temp.next;
+			}
+			this.size--;
+			return temp.data;
+		}
+
+		// O(n)
+		public int removeLast() throws Exception
+		{
+			if (this.size == 0)
+			{
+				throw new Exception("LinkedList is Empty");
+			}
+			Node temp = tail;
+			if (this.size == 1)
+			{
+				this.head = null;
+				this.tail = null;
+			} else
+			{
+				this.tail = this.getNodeAt(this.size - 2);
+				this.tail.next = null;
+			}
+			this.size--;
+			return temp.data;
+		}
+
+		// O(n)
+		public int removeAt(int index) throws Exception
+		{
+			if (index > size || index < 0)
+			{
+				throw new Exception(index + " Index Out of Bound Exception  " + this.size);
+			}
+
+			if (index == 0)
+			{
+				return this.removeFirst();
+			} else if (index == this.size - 1)
+			{
+				return this.removeLast();
+			} else
+			{
+				Node prevIndexNode = this.getNodeAt(index - 1);
+				Node currentNode = prevIndexNode.next;
+				prevIndexNode.next = currentNode.next;
+				this.size--;
+				return currentNode.data;
+			}
+		}
+
+		public void reverseData() throws Exception
+		{
+			if (this.size == 0)
+			{
+				throw new Exception("LinkedList is Empty");
+			}
+
+			if (this.size == 1)
+			{
+				return;
+			} else
+			{
+				int left = 0;
+				int right = this.size - 1;
+				while (left < right)
+				{
+					Node leftNode = getNodeAt(left);
+					Node rightNode = getNodeAt(right);
+
+					int temp = leftNode.data;
+					leftNode.data = rightNode.data;
+					rightNode.data = temp;
+					left++;
+					right--;
+				}
+			}
+		}
+
+		public void reversePointer() throws Exception
+		{
+			if (this.size == 0)
+			{
+				throw new Exception("LinkedList is Empty");
+			}
+
+			if (this.size == 1)
+			{
+				return;
+			} else
+			{
+				Node prev = this.head;
+				Node curr = prev.next;
+
+				while (curr != null)
+				{
+					Node ahead = curr.next;
+					curr.next = prev;
+					prev = curr;
+					curr = ahead;
+				}
+
+				// Swap head and tails
+				Node temp = this.head;
+				this.head = this.tail;
+				this.tail = temp;
+				tail.next = null;
+			}
+		}
+
+		public int getMid() throws Exception
+		{
+			if (this.size == 0)
+			{
+				throw new Exception("LinkedList is Empty");
+			}
+
+			Node slow = this.head;
+			Node fast = this.head;
+
+			while (fast.next != null && fast.next.next != null)
+			{
+				slow = slow.next;
+				fast = fast.next.next;
+			}
+			return slow.data;
+		}
+
+		public void kReverse(int k) throws Exception
+		{
+			if (this.size() <= 1 || k <= 1)
+			{
+				return;
+			}
+			int i = 0;
+			while (i < this.size())
+			{
+
+				int left = i;
+				int right = i + k - 1;
+				while (left < right)
+				{
+					Node leftNode = getNodeAt(left);
+					Node rightNode = getNodeAt(right);
+
+					int temp = leftNode.data;
+					leftNode.data = rightNode.data;
+					rightNode.data = temp;
+					left++;
+					right--;
+				}
+				i = i + k;
+			}
+
+		}
+
+		public void merge_sorted_list(LinkedList other) throws Exception
+		{
+			
+			Node head1 = this.head;
+			Node head2 = other.head;
+			
+			while(head1!=null && head2!=null)
+			{
+				if(head1.data>head2.data)
+				{
+				
+				}
+			}
+		}
+
+	}
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) throws Exception
+	{
+
+		int t = sc.nextInt();
+
+		while (t > 0)
+		{
+
+			LinkedList list1 = new LinkedList();
+			int n1 = sc.nextInt();
+
+			for (int j = 0; j < n1; j++)
+			{
+				int item = sc.nextInt();
+				list1.addLast(item);
+			}
+
+			LinkedList list2 = new LinkedList();
+			int n2 = sc.nextInt();
+
+			for (int j = 0; j < n2; j++)
+			{
+				int item = sc.nextInt();
+				list2.addLast(item);
+			}
+			list1.merge_sorted_list(list2);
+
+			t--;
+		}
+
+	}
+
+}
+
+/* https://github.com/shahiddhariwala */
