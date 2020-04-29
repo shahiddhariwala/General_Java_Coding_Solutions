db37fb6bf8254752b952bcce0ea3d099608d58dd
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-04-29 20:57:48 +0530

    Generic Heap

--- General_Java_Coding_Solutions/src/heap/HeapClient.java
+++ General_Java_Coding_Solutions/src/heap/HeapClient.java
@@ -6,15 +6,44 @@ Twitter  : https://twitter.com/shahiddhariwala
 
 package heap;
 
+import oops.generics.Car;
+
 public class HeapClient
 {
+	private static class Car implements Comparable<Car>
+	{
+
+		int speed;
+		int price;
+		String color;
+
+		public Car(int speed, int price, String color)
+		{
+			this.speed = speed;
+			this.price = price;
+			this.color = color;
+		}
+
+		@Override
+		public String toString()
+		{
+			return "{ S:" + this.speed + ", P:" + this.price + ", C:" + this.color + "}";
+		}
+
+		@Override
+		public int compareTo(Car other)
+		{
+
+			return this.price - other.price;
+		}
+	}
 
 	public static void main(String[] args)
 	{
 		Heap<Integer> h = new Heap<Integer>();
 		int arr[] =
 		{ 35, 33, 42, 10, 14, 19, 27, 44, 26, 31 };
-		for(Integer i : arr)
+		for (Integer i : arr)
 		{
 			h.add(i);
 		}
@@ -22,6 +51,29 @@ public class HeapClient
 		h.display();
 		System.out.println(h.remove());
 		h.display();
+
+		Car cars[] = new Car[5];
+		cars[0] = new Car(101, 5, "BLUE");
+		cars[1] = new Car(122, 1, "Yellow");
+		cars[2] = new Car(1033, 30, "Red");
+		cars[3] = new Car(104, 13, "White");
+		cars[4] = new Car(105, 3, "Purple");
+
+		Heap<Car> carHeap = new Heap<>();
+		for (Car i : cars)
+		{
+			carHeap.add(i);
+		}
+		carHeap.display();
+		/*
+		 * 10
+		 * 14 26 19 33 31 42 27 44 35
+		 * 14
+		 * 19 26 27 33 31 42 35 44
+		 * { S:122, P:1, C:Yellow} { S:105, P:3, C:Purple} { S:1033, P:30, C:Red} {
+		 * S:104, P:13, C:White} { S:101, P:5, C:BLUE}
+		 * 
+		 */
 	}
 
 }
