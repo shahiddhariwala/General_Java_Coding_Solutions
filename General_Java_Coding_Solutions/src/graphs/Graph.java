1aaf5e3b13740a60470e23b0776e53842ba38aa5
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-01 22:48:40 +0530

    Basicx Graph

---
+++ General_Java_Coding_Solutions/src/graphs/Graph.java
@@ -0,0 +1,126 @@
+/*
+Code by  : Shahid Dhariwala
+LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
+Twitter  : https://twitter.com/shahiddhariwala
+*/
+
+package graphs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Graph
+{
+	private class Vertex
+	{
+		HashMap<String, Integer> neighbors = new HashMap<>();
+	}
+
+	HashMap<String, Vertex> vertices;
+
+	public Graph()
+	{
+		vertices = new HashMap<String, Vertex>();
+	}
+
+	public int numVertex()
+	{
+		return this.vertices.size();
+	}
+
+	public boolean containsVertex(String vertexName)
+	{
+		return this.vertices.containsKey(vertexName);
+	}
+
+	public void addVertex(String vertexName)
+	{
+		Vertex vtx = new Vertex();
+		vertices.put(vertexName, vtx);
+	}
+
+	public void removeVertex(String vertexName)
+	{
+		Vertex currentVertex = this.vertices.get(vertexName);
+		if (currentVertex != null)
+		{
+			// if vertex exists
+			ArrayList<String> keys = new ArrayList<>(currentVertex.neighbors.keySet());
+			for (String key : keys)
+			{
+				this.vertices.get(key).neighbors.remove(vertexName);
+			}
+			this.vertices.remove(vertexName);
+		}
+	}
+
+	public int numEdges()
+	{
+		int count = 0;
+		Set<Map.Entry<String, Vertex>> entrySet = this.vertices.entrySet();
+		for (Map.Entry<String, Vertex> entry : entrySet)
+		{
+			count = count + entry.getValue().neighbors.size();
+		}
+		return count >> 1;
+	}
+
+	public boolean containsEdge(String parentVertex, String possibleNeighbor)
+	{
+		Vertex pv = this.vertices.get(parentVertex);
+		if (pv == null)
+		{
+			// Vertex doesnot exist
+			return false;
+		}
+		return pv.neighbors.containsKey(possibleNeighbor);
+	}
+
+	public void addEdge(String v1, String v2, int cost)
+	{
+		Vertex vertex1 = this.vertices.get(v1);
+		Vertex vertex2 = this.vertices.get(v2);
+		if (vertex1 == null || vertex2 == null || vertex1.neighbors.containsKey(v2))
+		{
+			// return
+			return;
+		}
+
+		vertex1.neighbors.put(v2, cost);
+		vertex2.neighbors.put(v1, cost);
+
+	}
+
+	public void removeEdge(String v1, String v2)
+	{
+		Vertex vertex1 = this.vertices.get(v1);
+		Vertex vertex2 = this.vertices.get(v2);
+		if (vertex1 == null || vertex2 == null || !vertex1.neighbors.containsKey(v2))
+		{
+			// return
+			return;
+		}
+
+		vertex1.neighbors.remove(v2);
+		vertex2.neighbors.remove(v1);
+
+	}
+
+	public void display()
+	{
+		System.out.println("---------------------------------------------------------------------");
+		System.out.println("Vertex\tNeighbors");
+		ArrayList<String> keys = new ArrayList<>(this.vertices.keySet());
+		for (String key : keys)
+		{
+			Vertex currentVertex = this.vertices.get(key);
+			System.out.println(key + " : \t" + currentVertex.neighbors);
+		}
+		System.out.println("---------------------------------------------------------------------");
+	}
+}
+
+/* https://github.com/shahiddhariwala */
---
+++ General_Java_Coding_Solutions/src/graphs/GraphClient.java
@@ -0,0 +1,108 @@
+/*
+Code by  : Shahid Dhariwala
+LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
+Twitter  : https://twitter.com/shahiddhariwala
+*/
+
+package graphs;
+
+public class GraphClient
+{
+
+	public static void main(String[] args)
+	{
+		Graph graph = new Graph();
+
+		// add vertex
+		graph.addVertex("A");
+		graph.addVertex("B");
+		graph.addVertex("C");
+		graph.addVertex("D");
+		graph.addVertex("E");
+		graph.addVertex("F");
+		graph.addVertex("G");
+
+		// add edge
+		graph.addEdge("A", "B", 2);
+		graph.addEdge("A", "D", 3);
+		graph.addEdge("B", "C", 4);
+		graph.addEdge("C", "D", 7);
+		graph.addEdge("D", "E", 10);
+		graph.addEdge("E", "F", 9);
+		graph.addEdge("E", "G", 8);
+		graph.addEdge("F", "G", 6);
+
+		// display
+		graph.display();
+		/*
+		 * ---------------------------------------------------------------------
+		 * Vertex Neighbors
+		 * A : {B=2, D=3}
+		 * B : {A=2, C=4}
+		 * C : {B=4, D=7}
+		 * D : {A=3, C=7, E=10}
+		 * E : {D=10, F=9, G=8}
+		 * F : {E=9, G=6}
+		 * G : {E=8, F=6}
+		 * ---------------------------------------------------------------------
+		 */
+
+		// numEdge numVertex
+		System.out.println(graph.numEdges());
+		System.out.println(graph.numVertex());
+		/*
+		 * 8
+		 * 7
+		 */
+
+		// containsEdge
+		System.out.println(graph.containsEdge("B", "G"));
+		System.out.println(graph.containsEdge("B", "C"));
+		/*
+		 * false
+		 * true
+		 */
+		// removeEdge
+		graph.removeEdge("B", "C");
+		graph.display();
+		System.out.println(graph.numEdges());
+		System.out.println(graph.numVertex());
+		/*
+		 * ---------------------------------------------------------------------
+		 * Vertex Neighbors
+		 * A : {B=2, D=3}
+		 * B : {A=2}
+		 * C : {D=7}
+		 * D : {A=3, C=7, E=10}
+		 * E : {D=10, F=9, G=8}
+		 * F : {E=9, G=6}
+		 * G : {E=8, F=6}
+		 * ---------------------------------------------------------------------
+		 * 7
+		 * 7
+		 */
+
+		// removeVertex
+		graph.removeVertex("C");
+		graph.display();
+		System.out.println(graph.numEdges());
+		System.out.println(graph.numVertex());
+		/*
+		 * ---------------------------------------------------------------------
+		 * Vertex Neighbors
+		 * A : {B=2, D=3}
+		 * B : {A=2}
+		 * D : {A=3, E=10}
+		 * E : {D=10, F=9, G=8}
+		 * F : {E=9, G=6}
+		 * G : {E=8, F=6}
+		 * ---------------------------------------------------------------------
+		 * 6
+		 * 6
+		 */
+
+	}
+
+}
+
+/* https://github.com/shahiddhariwala */
